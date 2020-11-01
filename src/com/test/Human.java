9d7f0487df1fe35e541eb2667c666e53c26855d0
hankerchen <25897684@qq.com>
2020-11-01 18:33:41 +0800

    java test project

---
+++ src/com/test/Human.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class Human {
+    private String name;
+    private int age;
+    public Human(String name){
+        this.name = name;
+        System.out.println("My name is " + this.name);
+    }
+}
